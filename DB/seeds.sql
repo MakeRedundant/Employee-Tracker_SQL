21850416a49d0aa393e10f6a1049ce7b232cbe18
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-12 22:06:15 +1000

    installed dotenv

--- DB/seeds.sql
+++ DB/seeds.sql
@@ -1,3 +1,4 @@
+-- Populate departments
 INSERT INTO department (name)
 VALUES  ('Marketing'), 
         ('Accounting'), 
@@ -6,19 +7,22 @@ VALUES  ('Marketing'),
         ('Human Resources'),
         ('Legal');
 
-
+-- Populate roles
 INSERT INTO role (title, salary, department_id) 
-VALUES  ('Marketing Manager', 100000, 1),
+VALUES  ('Marketing Manager', 102346, 1),
         ('Marketing Analyst', 55000, 1),
         ('Accounting Manager', 120000, 2),
         ('Accountant', 6000, 2),
-        ('Tech Lead', 150000, 3),
-        ('Software Engineer', 900000, 3),
-        ('HR Manager', 90, 5),
-        ('Recruiter', 50000, 5),
-        ('Corporate Lawyer', 100000, 6);
-        ('Front End Developer', 12345, 4);
+        ('Senior Engineer', 120000, 3), 
+        ('Mechanical Engineer', 80000, 3),
+        ('Tech Lead', 999999, 4),  
+        ('Front End Developer', 12345, 4), 
+        ('HR Manager', 900, 5),
+        ('Recruiter', 50, 5),
+        ('Judge', 56789, 6),
+        ('Corporate Lawyer', 10000, 6);
 
+-- Populate employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ('Chuck', 'Norris', 1, NULL),
         ('John', 'Doe', 2, 1),
@@ -28,5 +32,8 @@ VALUES  ('Chuck', 'Norris', 1, NULL),
         ('Marie', 'Curie', 6, 5),
         ('Leo', 'Dicaprio', 7, NULL),
         ('John', 'Wick', 8, 7),
-        ('Michael', 'Scott', 9, NULL);
-        ('Average', 'Joe', 10, 5);
+        ('Michael', 'Scott', 9, NULL),
+        ('Average', 'Joe', 10, 5),
+        ('Master', 'Chief', 11, NULL), 
+        ('Ash', 'Ketcham', 12, 11); 
+
