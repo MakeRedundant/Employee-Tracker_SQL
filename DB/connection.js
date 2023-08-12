3de3c43e21b0604800de03666579287cddd1d278
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-12 18:06:31 +1000

    added connectjoin.js, initial employee_db

---
+++ DB/connection.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const DB = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: process.env.PW,
+    database: "employee_db",
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+const pool = mysql.createPool(
+    {
+      host: "localhost",
+      user: "root",
+      password: process.env.PW,
+      database: "employee_db",
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    },
+    console.log(`Connected pool to the employee_db database.`)
+  );
+  
+  const promisePool = pool.promise();
+  
+  db.connect();
+  
+  // module.exports = db;
+  module.exports = { db: db, pool: pool, promisePool: promisePool };
+  
